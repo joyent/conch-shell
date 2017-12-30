9ad9f39717025507d6a0e460d2c7eb6e701436b3
sungo <sungo@sungo.us>
2017-12-30 18:22:11 -0500

    docs: add a bit of docs to "reports"

--- pkg/commands/internal/reports/init.go
+++ pkg/commands/internal/reports/init.go
@@ -3,6 +3,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// Package reports contains various read-only reports
 package reports
 
 import (
@@ -10,6 +12,7 @@ import (
 	"gopkg.in/jawher/mow.cli.v1"
 )
 
+// Init loads up the reports commands
 func Init(app *cli.Cli) {
 	app.Command(
 		"reports rep",
