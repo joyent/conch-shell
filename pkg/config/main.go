ba12dd41cfb5e32d2b67dd994446ca6ef08c7a73
sungo <sungo@sungo.us>
2017-12-30 18:52:22 -0500

    refactor: doc and delint config

--- pkg/config/main.go
+++ pkg/config/main.go
@@ -4,8 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// config wraps a conch shell config. Typically this is either coming from
-// and/or becoming JSON on disk.
+// Package config wraps a conch shell config. Typically this is either coming
+// from and/or becoming JSON on disk.
 package config
 
 import (
@@ -14,43 +14,44 @@ import (
 	"io/ioutil"
 )
 
-var ConchConfigNoPath = errors.New("No path found in config data")
+// ErrConfigNoPath is issued when a file operation is attempted on a
+// ConchConfig that lacks a path
+var ErrConfigNoPath = errors.New("No path found in config data")
 
+// ConchConfig represents the configuration information for the shell,
+// including the api server, user auth data, and a key/value store for
+// future-proofing
 type ConchConfig struct {
 	Path    string                 `json:"path"`
-	Api     string                 `json:"api"`
+	API     string                 `json:"api"`
 	User    string                 `json:"user"`
 	Session string                 `json:"session"`
 	KV      map[string]interface{} `json:"kv"`
 }
 
-// New() provides an initialized struct with default values geared towards a
+// New provides an initialized struct with default values geared towards a
 // dev environment. For instance, the default Api value is
 // "http://localhost:5001".
 func New() (c *ConchConfig) {
 	c = &ConchConfig{
-		Api: "http://localhost:5001",
+		API: "http://localhost:5001",
 	}
 	c.KV = make(map[string]interface{})
 	return c
 }
 
-// NewFromJson() unmarshals a JSON blob into a ConchConfig struct. It does
+// NewFromJSON unmarshals a JSON blob into a ConchConfig struct. It does
 // *not* fill in any default values.
-func NewFromJson(j string) (c *ConchConfig, err error) {
+func NewFromJSON(j string) (c *ConchConfig, err error) {
 	c = &ConchConfig{}
 
 	err = json.Unmarshal([]byte(j), c)
-
-	if err != nil {
-		return c, err
-	}
-	return c, nil
+	return c, err
 }
 
-// NewFromJsonFile reads a file off disk and unmarshals it into ConchConfig
+// NewFromJSONFile reads a file off disk and unmarshals it into ConchConfig
 // struct. It does *not* fill in any default values
-func NewFromJsonFile(path string) (c *ConchConfig, err error) {
+func NewFromJSONFile(path string) (c *ConchConfig, err error) {
 	c = &ConchConfig{}
 
 	raw, err := ioutil.ReadFile(path)
@@ -59,14 +60,10 @@ func NewFromJsonFile(path string) (c *ConchConfig, err error) {
 	}
 
 	err = json.Unmarshal(raw, c)
-	if err != nil {
-		return c, err
-	}
-
-	return c, nil
+	return c, err
 }
 
-// Serialize() marshals a ConchConfig struct into a JSON string
+// Serialize marshals a ConchConfig struct into a JSON string
 func (c *ConchConfig) Serialize() (s string, err error) {
 
 	j, err := json.MarshalIndent(c, "", "	")
@@ -78,11 +75,11 @@ func (c *ConchConfig) Serialize() (s string, err error) {
 	return string(j), nil
 }
 
-// SerializeToFile() marshals a ConchConfig struct into a JSON string and
+// SerializeToFile marshals a ConchConfig struct into a JSON string and
 // writes it out to the provided path
 func (c *ConchConfig) SerializeToFile(path string) (err error) {
 	if c.Path == "" {
-		return ConchConfigNoPath
+		return ErrConfigNoPath
 	}
 
 	j, err := json.MarshalIndent(c, "", "	")
@@ -92,9 +89,5 @@ func (c *ConchConfig) SerializeToFile(path string) (err error) {
 	}
 
 	err = ioutil.WriteFile(path, j, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
