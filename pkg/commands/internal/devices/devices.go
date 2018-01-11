103f4be5d31c83fcf0cf0f4b72a0afc4ffba21e8
sungo <sungo@joyent.com>
2018-01-11 16:10:41 -0500

    feat: "device get" now had a --full option that dumps out a ton of data instead of the small table

--- pkg/commands/internal/devices/devices.go
+++ pkg/commands/internal/devices/devices.go
@@ -11,16 +11,140 @@ import (
 	"github.com/joyent/conch-shell/pkg/util"
 	"github.com/joyent/go-conch"
 	"gopkg.in/jawher/mow.cli.v1"
+	uuid "gopkg.in/satori/go.uuid.v1"
+	"os"
 	"sort"
+	"text/template"
 )
 
+const singleDeviceTemplate = `
+Serial  {{ .D.ID }}
+Health: {{ .D.Health }}
+State:	{{ .D.State }}
+
+Location:
+  Datacenter: {{ .D.Location.Datacenter.Name }}
+  Rack: {{ .D.Location.Rack.Name }} - RU {{ .D.Location.Rack.Unit }}
+
+{{ if .D.AssetTag }}
+Asset Tag: {{ .D.AssetTag }}
+{{ end -}}
+
+Created:   {{ .D.Created.Local.Format .DF }}
+Last Seen: {{ .D.LastSeen.Local.Format .DF }}
+
+Graduated: {{ .D.Graduated.Local.Format .DF }}
+{{ if .TritonSetup }}
+Triton Setup: {{ .D.TritonSetup.Local.Format.DF }}
+Triton UUID:  {{ .D.TritonUUID }}
+{{ end -}}
+{{ with $r := .D.LatestReport }}
+Hardware:
+  {{ if $.HardwareMatches }}Model: {{ $.D.Location.TargetHardwareProduct.Alias }} // {{ $.D.Location.TargetHardwareProduct.Name }}{{ else }}Target Model: {{ $.D.Location.TargetHardwareProduct.Alias }} // {{ $.D.Location.TargetHardwareProduct.Name }}{{ end }}
+
+  BIOS Version: {{ $r.BiosVersion }}
+  CPU: {{ $r.Processor.count }}x {{ $r.Processor.type}}
+  RAM: {{ $r.Memory.total }}GB ({{ $r.Memory.count }} sticks)
+
+  NICS:{{range $k, $n := $r.Interfaces }}
+    - {{ $k }}{{ if $n.state }} - {{ $n.state }}{{ end }}{{ if $n.ipaddr }}
+      IP:   {{ $n.ipaddr }}{{ end }}
+      MAC:  {{ $n.mac }}{{ if $n.mtu }}
+      MTU:  {{ $n.mtu }}{{ end }}
+      Type: {{ $n.vendor }} {{ $n.product }}{{ if $n.peer_text }}
+      Peer: {{ $n.peer_text }}{{ end }}
+  {{ end }}
+  Disks:{{ range $k, $d := $r.Disks }}
+    - {{ $k }}{{ if $d.health }} - {{ $d.health }}{{ end }}
+      Device: {{ $d.device }}
+      Model:  {{ $d.model }}{{if $d.guid }}
+      GUID:   {{ $d.guid }}{{ end }}{{ if $d.drive_type }}
+      Type:   {{ $d.drive_type }}{{ end }}
+      Vendor: {{ $d.vendor }}{{ if $d.firmware }}
+      Firmware:  {{ $d.firmware }}{{ end }}{{ if $d.enclosure }}
+      Enclosure: {{ $d.enclosure }}{{ end }}{{ if $d.slot }}
+      Slot:   {{ $d.slot }}{{ end }}
+{{ end }}{{ end }}
+`
+
+const deviceValidationsTemplate = `
+  Validations:{{ if eq (len .) 0 }} NONE{{ else }}{{ range . }}
+    - {{ .ComponentType }} 
+      Component Name: {{ .ComponentName }}
+      Status: {{ if eq .Status 1 }}OK{{ else }}FAIL{{ end }}
+      Log: {{ .Log }}
+{{ end }}{{ end }}
+`
+
 func getOne(app *cli.Cmd) {
-	var fullOutput = app.BoolOpt("full", false, "When global --json is used, provide full data about the devices rather than normal truncated data")
+	var (
+		fullOutput      = app.BoolOpt("full", false, "Provide full data about the devices rather than normal truncated data")
+		showValidations = app.BoolOpt("validations", false, "When --full is used without --json, display a list of current validation reports")
+		showFailed      = app.BoolOpt("failed-validations", false, "When --full is used with --json, display a list of current validation reports, only those that failed. (Overrides --validations)")
+	)
+
 	app.Action = func() {
 		device, err := util.API.GetDevice(DeviceSerial)
 		if err != nil {
 			util.Bail(err)
 		}
+
+		if *fullOutput && !util.JSON {
+			t, err := template.New("device").Parse(singleDeviceTemplate)
+			if err != nil {
+				util.Bail(err)
+			}
+			hm := false
+			if uuid.Equal(
+				device.HardwareProduct,
+				device.Location.TargetHardwareProduct.ID,
+			) {
+				hm = true
+			}
+
+			data := struct {
+				D               conch.Device
+				TritonSetup     bool
+				HardwareMatches bool
+				DF              string
+			}{
+				device,
+				!device.TritonSetup.IsZero(),
+				hm,
+				util.DateFormat,
+			}
+
+			if err := t.Execute(os.Stdout, data); err != nil {
+				util.Bail(err)
+			}
+
+			if *showValidations || *showFailed {
+				t, err := template.New("validations").Parse(deviceValidationsTemplate)
+				if err != nil {
+					util.Bail(err)
+				}
+
+				validations := make([]conch.ValidationReport, 0)
+
+				if *showFailed {
+					for _, v := range device.Validations {
+						if v.Status == 0 {
+							validations = append(validations, v)
+						}
+					}
+
+				} else {
+					validations = device.Validations
+				}
+
+				if err := t.Execute(os.Stdout, validations); err != nil {
+					util.Bail(err)
+				}
+
+			}
+			return
+		}
+
 		devices := make([]conch.Device, 0)
 		devices = append(devices, device)
 
