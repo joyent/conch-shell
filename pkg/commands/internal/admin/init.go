ed86feaa01188f10b73826f19ee5c3e50c6ed554
sungo <sungo@joyent.com>
2018-05-14 14:27:58 -0400

    feat: introduce an "admin" command tree to initially hold the ability to revoke the tokens of a specific not-you user

---
+++ pkg/commands/internal/admin/init.go
@@ -0,0 +1,30 @@
+// Copyright Joyent, Inc.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// Package admin contains administrative commands for the conch api
+package admin
+
+import (
+	"github.com/joyent/conch-shell/pkg/util"
+	"gopkg.in/jawher/mow.cli.v1"
+)
+
+// Init loads up the commands
+func Init(app *cli.Cli) {
+	app.Command(
+		"admin",
+		"Commands for various server-side administrative tasks",
+		func(cmd *cli.Cmd) {
+			cmd.Before = util.BuildAPIAndVerifyLogin
+
+			cmd.Command(
+				"revoke-tokens",
+				"Revoke the auth tokens for a given user",
+				revokeTokens,
+			)
+		},
+	)
+}
