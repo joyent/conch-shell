b796a9175006c9243319c7ef8e89ed98317396fb
sungo <sungo@sungo.us>
2018-07-17 16:59:43 -0400

    Support CRUD for new-style hardware products (#113)

--- pkg/commands/internal/hardware/init.go
+++ pkg/commands/internal/hardware/init.go
@@ -24,7 +24,7 @@ var ProductUUID uuid.UUID
 func Init(app *cli.Cli) {
 	app.Command(
 		"hardware h",
-		"Commands for dealing with hardware",
+		"Commands for dealing with hardware products",
 		func(cmd *cli.Cmd) {
 			cmd.Before = func() {
 				util.BuildAPIAndVerifyLogin()
@@ -59,4 +59,64 @@ func Init(app *cli.Cli) {
 			)
 		},
 	)
+	app.Command(
+		"hardware_products hp",
+		"Commands for dealing with the new style hardware products",
+		func(cmd *cli.Cmd) {
+			cmd.Before = util.BuildAPIAndVerifyLogin
+
+			cmd.Command(
+				"products ps",
+				"Get a list of hardware products",
+				func(cmd *cli.Cmd) {
+					cmd.Command(
+						"get",
+						"Get all new-style hardware products",
+						getAllDB,
+					)
+
+					cmd.Command(
+						"create",
+						"Create a new hardware product",
+						createOneDB,
+					)
+				},
+			)
+
+			cmd.Command(
+				"product p",
+				"Deal with a single hardware product",
+				func(cmd *cli.Cmd) {
+					var productIDStr = cmd.StringArg("ID", "", "The UUID, name, or SKU of a hardware product")
+					cmd.Spec = "ID"
+
+					cmd.Before = func() {
+						ProductUUID, _ = util.MagicDBProductID(*productIDStr)
+						if uuid.Equal(ProductUUID, uuid.UUID{}) {
+							util.Bail(errors.New("Could not resolve the hardware product ID, name, or SKU"))
+						}
+					}
+
+					cmd.Command(
+						"get",
+						"Get a single hardware product",
+						getOneDB,
+					)
+
+					cmd.Command(
+						"delete rm",
+						"Delete a hardware product",
+						removeOneDB,
+					)
+
+					cmd.Command(
+						"update up",
+						"Update a hardware product",
+						updateOneDB,
+					)
+				},
+			)
+		},
+	)
+
 }
