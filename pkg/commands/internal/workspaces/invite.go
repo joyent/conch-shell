11a0803b0e0cec2f5e72ed0742b0bf2abeb721d5
sungo <sungo@sungo.us>
2018-08-22 16:52:05 -0400

    when inviting a user to a workspace, provide aliases and some logging aruond the newly changed role names

--- pkg/commands/internal/workspaces/invite.go
+++ pkg/commands/internal/workspaces/invite.go
@@ -1,6 +1,7 @@
 package workspaces
 
 import (
+	"fmt"
 	"github.com/jawher/mow.cli"
 	"github.com/joyent/conch-shell/pkg/util"
 )
@@ -8,16 +9,44 @@ import (
 func inviteUser(app *cli.Cmd) {
 	var (
 		emailArg = app.StringArg("EMAIL", "", "The email address of the user to be invited")
-		roleArg  = app.StringOpt("role", "Read-only", "The role for the new user")
+		roleArg  = app.StringOpt("role", "ro", "The role for the new user. Acceptable values are 'ro', 'rw', and 'admin'")
 	)
 
 	app.Spec = "EMAIL [OPTIONS]"
 
 	app.Action = func() {
+		var role string
+		switch *roleArg {
+		case "ro", "rw", "admin":
+			role = *roleArg
+		case "Read-only":
+			if !util.JSON {
+				fmt.Println("Role 'Read-only' is now called 'ro'. Will substitute the new value.")
+			}
+			role = "ro"
+		case "Administrator":
+			if !util.JSON {
+				fmt.Println("Role 'Adminstrator' is now called 'admin'. Will substitute the new value.")
+			}
+			role = "admin"
+
+		case "Integrator", "DC Operations", "Integrator Manager":
+			if !util.JSON {
+				fmt.Printf("Role '%s' is now called 'rw'. Will substitute the new value.\n", *roleArg)
+			}
+			role = "rw"
+
+		default:
+			if !util.JSON {
+				fmt.Println("Unknown role name. Falling back to 'ro'")
+			}
+			role = "ro"
+		}
+
 		err := util.API.InviteUser(
 			WorkspaceUUID,
 			*emailArg,
-			*roleArg,
+			role,
 		)
 
 		if err != nil {
