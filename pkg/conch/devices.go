b5a75472538405da45802f59501e47dff8959f26
sungo <sungo@joyent.com>
2019-01-09 12:17:48 -0500

    Pull the v2 "get" command's notion of an "ExtendedDevice" into the main lib, including the notion of validation plan executions
    (cherry picked from commit aad122c76a642fac4d777ef3a06011c6f23657d3)

--- pkg/conch/devices.go
+++ pkg/conch/devices.go
@@ -8,6 +8,7 @@ package conch
 
 import (
 	uuid "gopkg.in/satori/go.uuid.v1"
+	"sort"
 )
 
 // GetDevice returns a Device given a specific serial/id
@@ -15,6 +16,121 @@ func (c *Conch) GetDevice(serial string) (d Device, err error) {
 	d.ID = serial
 
 	return c.FillInDevice(d)
+}
+
+func (c *Conch) GetExtendedDevice(serial string) (ed ExtendedDevice, err error) {
+
+	d, err := c.GetDevice(serial)
+	if err != nil {
+		return ExtendedDevice{}, err
+	}
+
+	enclosures := make(map[string]map[int]Disk)
+	for _, disk := range d.Disks {
+		enclosure, ok := enclosures[disk.Enclosure]
+		if !ok {
+			enclosure = make(map[int]Disk)
+		}
+
+		if _, ok := enclosure[disk.Slot]; !ok {
+			enclosure[disk.Slot] = disk
+		}
+
+		enclosures[disk.Enclosure] = enclosure
+	}
+
+	/***********/
+
+	ed = ExtendedDevice{
+		Device:        d,
+		IPMI:          "",
+		HardwareName:  "",
+		SKU:           "",
+		Enclosures:    enclosures,
+		IsGraduated:   !d.Graduated.IsZero(),
+		IsTritonSetup: !d.TritonSetup.IsZero(),
+		IsValidated:   !d.Validated.IsZero(),
+		Validations:   make([]ValidationPlanExecution, 0),
+	}
+
+	allValidations := make(map[uuid.UUID]Validation)
+	serverValidations, err := c.GetValidations()
+	if err != nil {
+		return ed, err
+	}
+	for _, v := range serverValidations {
+		allValidations[v.ID] = v
+	}
+
+	if validationStates, err := c.DeviceValidationStates(d.ID); err == nil {
+
+		plans := make([]ValidationPlanExecution, 0)
+
+		for _, state := range validationStates {
+			name := "[unknown]"
+			validationPlan, err := c.GetValidationPlan(state.ValidationPlanID)
+			if err == nil {
+				name = validationPlan.Name
+			}
+
+			byValidationID := make(map[uuid.UUID][]ValidationResult)
+
+			for _, result := range state.Results {
+				results, ok := byValidationID[result.ValidationID]
+				if !ok {
+					results = make([]ValidationResult, 0)
+				}
+
+				results = append(results, result)
+				byValidationID[result.ValidationID] = results
+			}
+
+			runs := make(ValidationRuns, 0)
+			for id, results := range byValidationID {
+				var run ValidationRun
+				run.ID = id
+				run.Name = "[unknown]"
+				if v, ok := allValidations[id]; ok {
+					run.Name = v.Name
+				}
+				passed := true
+				for _, result := range results {
+					if result.Status != "pass" {
+						passed = false
+					}
+				}
+
+				run.Passed = passed
+				run.Results = results
+				runs = append(runs, run)
+			}
+
+			sort.Sort(runs)
+			plans = append(plans, ValidationPlanExecution{
+				ID:          state.ValidationPlanID,
+				Name:        name,
+				Validations: runs,
+			})
+
+		}
+
+		ed.Validations = plans
+	}
+
+	ipmi, err := c.GetDeviceIPMI(d.ID)
+	if err == nil {
+		ed.IPMI = ipmi
+	}
+
+	if !uuid.Equal(d.HardwareProduct, uuid.UUID{}) {
+		hp, err := c.GetHardwareProduct(d.HardwareProduct)
+		if err == nil {
+			ed.HardwareName = hp.Name
+			ed.SKU = hp.SKU
+		}
+	}
+
+	return ed, nil
 }
 
 // FillInDevice takes an existing device and fills in its data using "/device"
