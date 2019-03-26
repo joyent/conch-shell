ed7cf555a1c2edd4d354318a6936f38171256183
sungo <sungo@joyent.com>
2019-03-25 20:00:29 -0400

    build| reworks the dockerfile.release and release script so that the release build runs as the same UID as the current user so that docker doesnt build or drop release binaries as root

--- docker/release.bash
+++ docker/release.bash
@@ -21,16 +21,18 @@ fi
 
 IMAGE_NAME="${PREFIX}/${NAME}:${LABEL}"
 
-mkdir release
+mkdir -p release
 
 docker build \
 	-t ${IMAGE_NAME} \
 	--build-arg BRANCH=${BRANCH} \
+	--build-arg UID=$(id -u) \
 	--file Dockerfile.release . \
 && \
 docker run --rm \
 	--name ${BUILDER}_${BUILDNUMBER} \
-	--mount type=bind,source="${PWD}/release",target="/go/src/github.com/joyent/conch-shell/release" \
+	-u $(id -u):$(id -g) \
+	--mount type=bind,source="${PWD}/release",target="/home/app/go/src/github.com/joyent/conch-shell/release" \
 	${IMAGE_NAME};
 
 docker rmi ${IMAGE_NAME}
